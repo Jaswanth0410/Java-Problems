7a9cb68cf3802e7af0efb432b33cb3977ac8f0ad
Jaswanth <139313622+Jaswanth0410@users.noreply.github.com>
2024-06-19 12:29:28 +0530

    unary.java

---
+++ unary.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Intro{
+    public static void main(String args[]){
+        // int num =1234;
+        // System.out.printf("%15d",num);
+        int num=20;
+        System.out.printf("%d %d %d %d",num++,num,num++,++num);
+    }
+}
