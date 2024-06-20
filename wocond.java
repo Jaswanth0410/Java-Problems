81ccffe2b54ad13fc1b23556c2f52d288760c654
Jaswanth <139313622+Jaswanth0410@users.noreply.github.com>
2024-06-20 10:33:41 +0530

    wocond.java

---
+++ wocond.java
@@ -0,0 +1,10 @@
+//Without using condition print the input:1  output:2,input:2 output 1
+import java.util.*;
+public class Intro {
+    public static void main(String[] args) {
+        int num;
+        Scanner input = new Scanner(System.in);
+        num = input.nextInt();
+        System.out.println(3 - num);
+    }
+}
