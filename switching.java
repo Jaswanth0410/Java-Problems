f2d5c67987dc73211e4475227ef0ad84155c99c4
Jaswanth <139313622+Jaswanth0410@users.noreply.github.com>
2024-06-22 11:00:15 +0530

    switching.java

---
+++ switching.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class Main{
+    public static void main(String[] args) {
+        int num = 4;
+        switch(num)
+        {
+            case 1 :System.out.println("Jelo");
+            break;
+            case 2 :System.out.println("Hai");
+            break;
+            case 3 : System.out.println("World");
+            break;
+            default :System.out.println("Deafult");
+        }
+     }
+    }
