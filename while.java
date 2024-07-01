d10ada7f20e617cddaa47dea4387dac1df27593b
Jaswanth <139313622+Jaswanth0410@users.noreply.github.com>
2024-07-01 10:27:09 +0530

    while.java

---
+++ while.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Intro{
+    public static void main(String args[]){
+        // Scanner input = new Scanner (System.in);
+        int itr = 1;
+        while(itr<=5){
+            System.out.print("\nHello");
+            itr+=1;
+        }
+    }
+}
