81b70e27e539ecb37281b71193a9994be5ea70e1
Jaswanth <139313622+Jaswanth0410@users.noreply.github.com>
2024-07-01 10:28:21 +0530

    dowhile.java

---
+++ dowhile.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Intro{
+    public static void main(String args[]){
+        // Scanner input = new Scanner (System.in);
+        int itr = 6;
+        do{
+            System.out.print("\nHello");
+            itr+=1;
+        }while(itr<=5);
+    }
+}
