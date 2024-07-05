e078ce37daa1453a09c3a3a9ead6bb61625335ef
Jaswanth <139313622+Jaswanth0410@users.noreply.github.com>
2024-07-05 15:07:54 +0530

    rightpyr.java

---
+++ rightpyr.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        int N,row,col;
+        N = sc.nextInt();
+        for(row=1;row<=N;row++)
+        {
+            for(col=1;col<=N;col++)
+            {
+                if(col<=N-row)
+                System.out.print(" ");
+                else
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
