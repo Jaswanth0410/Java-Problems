f345af79c84dd6e1330fb5f0d121ac864b1f48de
Jaswanth <139313622+Jaswanth0410@users.noreply.github.com>
2024-07-13 12:22:38 +0530

    digitswap.java

---
+++ digitswap.java
@@ -0,0 +1,19 @@
+import java.util.*;
+public class Main{
+    public static void main(String args[]){
+        Scanner input = new Scanner(System.in);
+        // String str = input.nextLine();
+        int num,pos1,pos2,pv1,pv2,dig1,dig2;
+        num = input.nextInt();
+        pos1 = input.nextInt();
+        pos2 = input.nextInt();
+        pv1 = (int)Math.pow(10,pos1-1);
+        pv2 = (int)Math.pow(10,pos2-1);
+        dig = (num/pv1)%10;
+        dig2 = (num/pv2)%10;
+        num = num-(dig1*pv1)-(dig2*pv2);
+        num = num+(dig1*pv2)+(dig2*pv1);
+        System.out.print(num);
+        
+    }
+}
